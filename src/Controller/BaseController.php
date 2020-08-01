f2c8b92f050c9640c398ea31c768a341f929c36f
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-08-01 09:22:41 +0200

    Pogodynka v0.0.0.1

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Handler\ImgwHandler;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    private $logger;
+
+    private $imgwHandler;
+
+    public function __construct(LoggerInterface $logger, ImgwHandler $imgwHandler)
+    {
+        $this->logger = $logger;
+        $this->imgwHandler = $imgwHandler;
+    }
+
+    /**
+     * @Route("/", defaults={"_format"="html"}, name="index")
+     */
+    public function indexAction(Request $request, string $_format): Response
+    {
+        return $this->render(
+            'base.' . $_format . '.twig'
+        );
+    }
+
+    /**
+     * @Route("/szczecin", defaults={"_format"="html"}, name="szczecin")
+     */
+    public function szczecinAction(Request $request, string $_format): Response
+    {
+        $weatherData = $this->imgwHandler->getData('szczecin');
+
+        return $this->render(
+            'szczecin.' . $_format . '.twig',
+            [
+                'temperature' => $weatherData['temperatura'],
+                'windSpeed' => $weatherData['predkosc_wiatru'],
+                'windDirection' => $weatherData['kierunek_wiatru'],
+                'pressure' => $weatherData['cisnienie'],
+            ]
+        );
+    }
+}
---
+++ src/Handler/ImgwHandler.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Handler;
+
+use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
+
+class ImgwHandler
+{
+    const IMGW_BASE_URL = 'https://danepubliczne.imgw.pl/api/data/synop/station/';
+
+    private $client;
+
+    private $logger;
+
+    public function __construct(Client $client, LoggerInterface $logger)
+    {
+        $this->client = $client;
+        $this->logger = $logger;
+    }
+
+    public function getData(string $city): array
+    {
+        $this->logger->info(
+            'Pobieranie danych o pogodzie z wybranego miasta',
+            compact('city')
+        );
+
+        $response = $this->client->request(
+            'GET',
+            self::IMGW_BASE_URL . $city,
+        );
+
+        if ($response->getStatusCode() !== 200) {
+            $this->logger->error(
+                'Błąd pobierania danych o pogodzie w mieście',
+                compact('city')
+            );
+
+            throw new \Exception('Błąd pobierania danych');
+        }
+
+        $this->logger->info(
+            'Pobrane dane o pogodzie w mieście',
+            compact('city', 'response')
+        );
+
+        return $this->parseData(
+            json_decode(
+                $response->getBody(),
+                true
+            )
+        );
+    }
+
+    private function parseData(array $data): array
+    {
+        $data['kierunek_wiatru'] = $this->getWindDirection($data['kierunek_wiatru']);
+
+        return $data;
+    }
+
+    private function getWindDirection(int $windDirectionInDegrees): string
+    {
+        if ($windDirectionInDegrees > 315 || $windDirectionInDegrees < 45) {
+            return 'N';
+        }
+
+        if ($windDirectionInDegrees >= 45 && $windDirectionInDegrees <= 135) {
+            return 'E';
+        }
+
+        if ($windDirectionInDegrees >= 135 && $windDirectionInDegrees <= 225) {
+            return 'S';
+        }
+
+        if ($windDirectionInDegrees >= 225 && $windDirectionInDegrees <= 315) {
+            return 'W';
+        }
+    }
+}
