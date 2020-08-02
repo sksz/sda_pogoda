098ce541e8efe81817df4a7721e0207d55fbba56
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-08-02 10:04:04 +0200

    Doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
